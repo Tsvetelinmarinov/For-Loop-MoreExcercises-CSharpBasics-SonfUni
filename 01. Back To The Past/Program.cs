19c83895f8269eb69a1e608e232a081f273be5d4
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-27 19:58:10 +0200

    Add project files.

---
+++ 01. Back To The Past/Program.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * 01. Back To The Past
+ */
+
+//•	Наследените пари – реално число в интервала [1.00 ... 1 000 000.00]
+//•	Годината, до която трябва да живее (включително) – цяло число в интервала [1801 ... 1900]
+
+double legacy = double.Parse(Console.ReadLine());
+int year = int.Parse(Console.ReadLine());
+
+int age = 17;
+
+//Като приемем, че за всяка четна (1800, 1802 и т.н.) година ще харчи 12 000 лева.
+//За всяка нечетна (1801,1803  и т.н.) ще харчи 12 000 + 50 * [годините, които е навършил през дадената година].
+
+for (int i = 1800; i <= year; i++)
+{
+    if (i % 2 == 0) 
+    { 
+        legacy -= 12000; 
+        age++; 
+    }
+    else 
+    { 
+        legacy -= 12000 + 50 * ++age;
+    }
+}
+
+//•	Ако парите са достатъчно:
+//o   "Yes! He will live a carefree life and will have {N} dollars left." – където N са парите, които ще му останат.
+//•	Ако парите НЕ са достатъчно:
+//o   "He will need {М} dollars to survive." – където M е сумата, която НЕ достига
+
+if (legacy > 0)
+{
+    Console.WriteLine($"Yes! He will live a carefree life and will have {legacy:F2} dollars left.");
+}
+else
+{
+    Console.WriteLine($"He will need {Math.Abs(legacy):F2} dollars to survive.");
+}
---
+++ 02. Hospital/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * 02. Hospital
+ */
+
+//•	На първия ред – периода, за който трябва да направите изчисления. Цяло число в интервала [1 ... 1000]
+//•	На следващите редове(равни на броят на дните) – броя пациенти, които пристигат за преглед за текущия ден. Цяло число в интервала [0…10 000]
+
+int days = int.Parse(Console.ReadLine());
+
+int acceptPacients = 0, sendedPacients = 0, doctors = 7, daysCounter = 3;
+
+//4
+//7
+//27
+//9
+//1
+
+for (int i = 1; i <= days; i++)
+{
+    int pacients = int.Parse(Console.ReadLine());
+
+    if (i == 3 || i == 6 || i == 9 || i == 12)
+    {
+        if (sendedPacients > acceptPacients) { doctors++; }
+    }
+
+    if (pacients <= doctors) 
+    { 
+        acceptPacients += pacients; 
+    }
+    else 
+    { 
+        acceptPacients += doctors;
+        sendedPacients += pacients - doctors;
+    }
+}
+
+//За даден период от време, всеки ден в болницата пристигат пациенти за преглед. Тя разполага първоначално със 7 лекари. 
+//Всеки лекар може да преглежда само по един пациент на ден, но понякога има недостиг на лекари,
+//затова останалите пациенти се изпращат в други болници. Всеки трети ден, болницата прави изчисления и ако броят на непрегледаните пациенти е 
+//по-голям от броя на прегледаните, се назначава още един лекар. Като назначаването става преди да започне приемът на пациенти за деня.
+
+//•	На първия ред: "Treated patients: {брой прегледани пациенти}."
+//•	На втория ред: "Untreated patients: {брой непрегледани пациенти}."
+
+Console.WriteLine($"Treated patients: {acceptPacients}.");
+Console.WriteLine($"Untreated patients: {sendedPacients}.");
